c01553f96cf0b43a5f19ca2ca75d612de2ba2cec
Sam Coy <samcoy3@gmail.com>
2019-12-02 14:39:18 +0000

    Added even more expressive typing
    Also factored out the operations

--- src/Intcode.hs
+++ src/Intcode.hs
@@ -7,6 +7,7 @@ import Data.Ix
 
 type MemoryState = Array Int Int
 type ProgramState = (Int, MemoryState)
+type Operation = ProgramState -> ProgramState
 
 readIntcode :: String -> MemoryState
 readIntcode = (\x -> listArray (0, (length x) - 1) x)
@@ -15,8 +16,8 @@ readIntcode = (\x -> listArray (0, (length x) - 1) x)
 
 runIntcode' :: ProgramState -> ProgramState
 runIntcode' (p, a) = case a ! p of
-  1 -> runIntcode' ((p + 4), (a // [(a ! (p + 3), (a !* (p + 1)) + (a !*(p + 2)))]))
-  2 -> runIntcode' ((p + 4), (a // [(a ! (p + 3), (a !* (p + 1)) * (a !* (p + 2)))]))
+  1 -> runIntcode' . addOp $ (p, a)
+  2 -> runIntcode' . multOp $ (p, a)
   99 -> (p, a)
   _ -> error "Invalid opcode"
 
@@ -25,3 +26,17 @@ runIntcode arr = snd . runIntcode' $ (0, arr)
 
 programOutput :: MemoryState -> Int
 programOutput = (flip (!) 0)
+
+-- OPERATIONS
+
+addOp :: Operation
+addOp (p, a) = (p', a') where
+  p' = p + 4
+  value = (a !* (p + 1)) + (a !* (p + 2))
+  a' = a // [(a ! (p + 3), value)]
+
+multOp :: Operation
+multOp (p, a) = (p', a') where
+  p' = p + 4
+  value = (a !* (p + 1)) * (a !* (p + 2))
+  a' = a // [(a ! (p + 3), value)]
